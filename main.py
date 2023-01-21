97bfbc50b81d2ec1532d723d2c92c9ff08dfe23b
Eduard Kolotushin <kolotushin.ev@phystech.edu>
2023-01-21 20:30:22 +0300

    Add changes

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import telebot
 from telebot import types
 from config import Config
+import re
 
 TGTOKEN = Config.TGTOKEN
 bot = telebot.TeleBot(TGTOKEN)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start(message):
-    markup = types.InlineKeyboardMarkup()
-    btn = types.InlineKeyboardButton(text='My VK',
-                                     url='https://vk.com/eduard_kolotushin')
-    markup.add(btn)
-    bot.send_message(message.from_user.id, "Мой вк...", reply_markup=markup)
+    # markup = types.InlineKeyboardMarkup()
+    # btn = types.InlineKeyboardButton(text='This is my first own telegram bot...',
+    #                                  url='')
+    # markup.add(btn)
+    bot.send_message(message.from_user.id, "This is my first own telegram bot...")
+
+
+@bot.message_handler(content_types=['text'])
+def reply_to_text(message):
+    txt = message.text
+    if len(re.findall(r'linux', txt, re.I)) > 0:
+        bot.send_message(message.from_user.id, "Linux related operation...")
+    else:
+        bot.send_message(message.from_user.id, "Non linux related operation...")
 
 
 bot.polling(none_stop=True, interval=0)
