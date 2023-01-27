b1532d8e1f5591d0e86536d49f5a223a177027b5
Eduard Kolotushin <kolotushin.ev@phystech.edu>
2023-01-27 20:53:51 +0300

    Add voice message support

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import datetime
 import telebot
-from telebot import types
 from config import Config
 import openai
 from utils.audio_converter import convert_audio_to_text
 from utils.manager import History, Manager, Base
-from utils import engine
 import asyncio
 import logging
 from threading import Thread
-import re
 import os
 
 TGTOKEN = Config.TGTOKEN
 GPTTOKEN = Config.GPTTOKEN
 FILEFOLDER = Config.FILEFOLDER
 
-# loop = asyncio.new_event_loop()
-# asyncio.set_event_loop(loop)
 bot = telebot.TeleBot(TGTOKEN)
 openai.api_key = GPTTOKEN
 manager = Manager()
@@ -27,20 +22,14 @@ logger.setLevel("INFO")
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-    # markup = types.InlineKeyboardMarkup()
-    # btn = types.InlineKeyboardButton(text='This is my first own telegram bot...',
-    #                                  url='')
-    # markup.add(btn)
     bot.send_message(message.from_user.id, "This is my first own telegram bot...")
 
 
 @bot.message_handler(content_types=['text'])
 def reply_to_text(message):
     txt = message.text
-    # print(txt)
     logger.info(txt)
     history_txt = manager.extract_dialog(message.from_user.id)
-    # print(history_txt)
     txt_full = txt
     logger.info(txt_full)
     if history_txt is not None:
@@ -66,29 +55,21 @@ def reply_to_text(message):
 # handler for audio from telegram
 @bot.message_handler(content_types=['audio'])
 def reply_to_audio(message):
-    # print(message)
     file_info = bot.get_file(message.audio.file_id)
-    # print(file_info)
     downloaded_file = bot.download_file(file_info.file_path)
-    # print(downloaded_file)
     with open(os.path.join(FILEFOLDER, "audio.ogg"), 'wb') as new_file:
         new_file.write(downloaded_file)
-    # print("file saved")
     bot.send_message(message.from_user.id, "file saved")
 
 
 # handler for voice from telegram
 @bot.message_handler(content_types=['voice'])
 def reply_to_voice(message):
-    # print(message)
     file_info = bot.get_file(message.voice.file_id)
-    # print(file_info)
     downloaded_file = bot.download_file(file_info.file_path)
-    # print(downloaded_file)
     with open(os.path.join(FILEFOLDER, "voice.ogg"), 'wb') as new_file:
         new_file.write(downloaded_file)
     text = convert_audio_to_text(FILEFOLDER, "voice.ogg")
-    # print("file saved")
     resp = openai.Completion.create(
         model="text-davinci-003",
         prompt=text,
--- utils/audio_converter.py
+++ utils/audio_converter.py
@@ -12,12 +12,9 @@ def convert_audio_to_text(folder, file_name):
         audio = r.record(source)  # read the entire audio file
     try:
         text = r.recognize_google(audio, language="ru")
-        print("Google Speech Recognition thinks you said " + text)
     except sr.UnknownValueError:
-        # print("Google Speech Recognition could not understand audio")
         text = "Google Speech Recognition could not understand audio"
     except sr.RequestError as e:
-        # print("Could not request results from Google Speech Recognition service; {0}".format(e))
         text = "Could not request results from Google Speech Recognition service; {0}".format(e)
     return text
 
